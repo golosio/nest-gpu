b8a3e61172b3f298d5cef952ddc6ee920d1e7c7b
Bruno Golosio <golosio@unica.it>
2024-05-21 23:05:34 +0200

    Debugging

--- src/copass_sort.h
+++ src/copass_sort.h
@@ -524,7 +524,7 @@ copass_sort::sort_template( KeyArrayT key_array,
 	int h_arg_max;
 	gpuErrchk( cudaMemcpy( &h_arg_max, d_arg_max, sizeof( int ), cudaMemcpyDeviceToHost ) );
 	printf( "okST9b i_sub %d tmp_idx %d h_arg_max %d h_max_diff %ld\n", i_sub, tmp_idx, h_arg_max, h_max_diff);
-	if (tmp_idx==199) {
+	if (tmp_idx==200) { // 200 o 199
 	  position_t h_m_d[k];
 	  position_t h_m_u[k];
 	  gpuErrchk( cudaMemcpy( h_m_d, d_m_d, k*sizeof( position_t ), cudaMemcpyDeviceToHost ) );
